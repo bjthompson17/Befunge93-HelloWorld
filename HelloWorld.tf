0f6b9d13d2890e1334fe956a56b9cff3de222ebd
bjthompson17 <tho17054@byui.edu>
2026-04-23 21:24:55 -0600

    Update to program to handle period as well as newline to end input

--- HelloWorld.tf
+++ HelloWorld.tf
@@ -1,14 +1,12 @@
-0104p                                  v
+0101p                                  v
+;Variable Space
 v"Hello World!"*52"What is your name? "<
->:vv      <
-^,_>~:52*-|
-1
-v    p4g40_v
->104g+04p:^
-v"Hello "  <
+>:vv            _ v
+^,_>~::52*-\"."-|    ;Checks for '.' or '\n' to end input
+                >$v
+        v    p1g10_v ;Stores name in Variable Space
+        >101g+01p:^
+v"Hello "          <
 >:vv           ,<
-^,_>04g1-:04p4g:|           
+^,_>01g1-:01p1g:|    ;Retreive name from Variable Space
        @,,"!"*25<
-         
-
-       
